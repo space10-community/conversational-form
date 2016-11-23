31e0264edf3a0a26f640d72e70638828403289a8
Felix Nielsen <felix@flexmotion.dk>
2016-11-23 17:35:26 +0000

    fix gulp build order

--- gulp-tasks/styles.js
+++ gulp-tasks/styles.js
@@ -7,7 +7,6 @@ var gutil = require('gulp-util');
 var livereload = require('gulp-livereload');
 var notify = require("gulp-notify");
 var concat = require('gulp-concat');
-var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
 
 function swallowError(error) {
@@ -45,6 +44,18 @@ global.gulp.task('stylus', function() {
 global.gulp.task('styles-build', ['stylus'], function(){
 	var src = [
 		global.buildFolder + "**/*.css",
+		global.buildFolder + "cf/cf.css",
+		global.buildFolder + "cf/ui/control-elements/cf-control-elements.css",
+		global.buildFolder + "cf/ui/control-elements/cf-button.css",
+		global.buildFolder + "cf/ui/control-elements/cf-radio-button.css",
+		global.buildFolder + "cf/ui/control-elements/cf-checkbox-button.css",
+		global.buildFolder + "cf/ui/control-elements/cf-options-list.css",
+		global.buildFolder + "cf/ui/control-elements/cf-upload-file-ui.css",
+		global.buildFolder + "cf/ui/cf-input.css",
+		global.buildFolder + "cf/ui/cf-info.css",
+		global.buildFolder + "cf/ui/cf-list-button.css",
+		global.buildFolder + "cf/ui/chat/cf-chat-response.css",
+		global.buildFolder + "cf/ui/chat/cf-chat.css",
 		"!" + global.buildFolder + "conversational-form.css",
 		"!" + global.distFolder + "conversational-form.min.css",
 	]
@@ -52,7 +63,6 @@ global.gulp.task('styles-build', ['stylus'], function(){
 	var stream = global.gulp.src(src)
 		.pipe(concat('conversational-form.css'))
 		.pipe(global.gulp.dest(global.buildFolder))
-		.pipe(cssmin())
 		.pipe(rename({suffix: '.min'}))
 		.pipe(global.gulp.dest(global.distFolder));
 	
