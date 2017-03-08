31c07d4ccb3ee2115a3c810d3531079d7acc099f
Felix Nielsen <felix@flexmotion.dk>
2017-03-08 18:54:24 +0000

    flows done

--- docs/src/scripts/cf/ConversationalFormDocs.ts
+++ docs/src/scripts/cf/ConversationalFormDocs.ts
@@ -11,11 +11,48 @@ namespace cf {
 		* @type HTMLElement
 		*/
 		private el: HTMLElement;
+		private introTimer: any = 0;
 		constructor(){
 			this.el = document.getElementById("conversational-form-docs");
-			setTimeout(() => {
-				document.getElementById("form").classList.add('show');
-			}, 2000);
+
+			const isMenuVisible = window.getComputedStyle(document.getElementById("small-screen-menu")).getPropertyValue("display") != "none";
+			if(isMenuVisible)
+				this.introFlow1();
+			else
+				this.introFlow2();
+		}
+
+		/**
+		* @name introFlow1
+		* flow for small screens
+		*/
+		private introFlow1(): void {
+			this.introTimer = setTimeout(() => {
+				this.toggleMenuState();
+
+				this.introTimer = setTimeout(() => {
+					this.toggleConversation()
+				}, 2500);
+			}, 500);
+		}
+
+		/**
+		* @name introFlow2
+		* flow for larger screens
+		*/
+		private introFlow2(): void {
+			this.introTimer = setTimeout(() => {
+				document.getElementById("info").classList.add('show');
+
+				this.introTimer = setTimeout(() => {
+					document.getElementById("form").classList.add('show');
+					document.getElementById("cf-toggle-btn").classList.add('show');
+
+					this.introTimer = setTimeout(() => {
+						this.toggleConversation()
+					}, 1500);
+				}, 1500);
+			}, 500);
 		}
 
 		public toggleMenuState(){
@@ -29,6 +66,8 @@ namespace cf {
 		}
 
 		public toggleConversation(){
+			clearTimeout(this.introTimer);
+
 			const open = this.el.classList.toggle('cf-toggle', !this.el.classList.contains('cf-toggle'));
 
 			if(open){
