1a94bd2cd05af09193bb18ef95d770c2ccbf269a
Felix Nielsen <felix@flexmotion.dk>
2017-03-09 10:05:57 +0000

    submit form flow

--- docs/src/scripts/cf/ConversationalFormDocs.ts
+++ docs/src/scripts/cf/ConversationalFormDocs.ts
@@ -91,8 +91,29 @@ class ConversationalFormDocs{
 					context: document.getElementById("cf-context"),
 					robotImage: "data:image/svg+xml;charset=utf-8;base64,PHN2ZyB3aWR0aD0iMjBweCIgaGVpZ2h0PSIyMHB4IiB2aWV3Qm94PSIwIDAgMjAgMjAiIHZlcnNpb249IjEuMSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayI+CiAgICA8ZyBzdHJva2U9Im5vbmUiIHN0cm9rZS13aWR0aD0iMSIgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj4KICAgICAgICA8ZyB0cmFuc2Zvcm09InRyYW5zbGF0ZSgtNzY0LjAwMDAwMCwgLTUzMC4wMDAwMDApIiBmaWxsPSIjMjIyMjIyIj4KICAgICAgICAgICAgPGcgdHJhbnNmb3JtPSJ0cmFuc2xhdGUoNzUzLjAwMDAwMCwgNTE5LjAwMDAwMCkiPgogICAgICAgICAgICAgICAgPHJlY3QgeD0iMTEiIHk9IjExIiB3aWR0aD0iMjAiIGhlaWdodD0iMjAiPjwvcmVjdD4KICAgICAgICAgICAgPC9nPgogICAgICAgIDwvZz4KICAgIDwvZz4KPC9zdmc+",
 					userImage: "data:image/svg+xml;charset=utf-8;base64,PHN2ZyB3aWR0aD0iMjBweCIgaGVpZ2h0PSIxNnB4IiB2aWV3Qm94PSIwIDAgMjAgMTYiIHZlcnNpb249IjEuMSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayI+CiAgICA8ZyBzdHJva2U9Im5vbmUiIHN0cm9rZS13aWR0aD0iMSIgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj4KICAgICAgICA8ZyB0cmFuc2Zvcm09InRyYW5zbGF0ZSgtMTM3MS4wMDAwMDAsIC02MTAuMDAwMDAwKSIgZmlsbD0iI0ZGRkZGRiI+CiAgICAgICAgICAgIDxnIHRyYW5zZm9ybT0idHJhbnNsYXRlKDEyNzQuMDAwMDAwLCA1OTkuMDAwMDAwKSI+CiAgICAgICAgICAgICAgICA8cG9seWdvbiBwb2ludHM9IjEwNyAxMSAxMTcgMjcgOTcgMjciPjwvcG9seWdvbj4KICAgICAgICAgICAgPC9nPgogICAgICAgIDwvZz4KICAgIDwvZz4KPC9zdmc+",
-					submitCallback: () =>{
-						this.cf.addRobotChatResponse("greeeatt!");
+					submitCallback: () => {
+						
+					},
+					flowStepCallback: (dto: any, success: () => void, error: () => void,) => {
+						if(dto.input.currentTag.domElement){
+							if(dto.input.currentTag.domElement.getAttribute("name") == "repeat"){
+								location.reload();
+							}else if(dto.input.currentTag.domElement.getAttribute("name") == "submit-form"){
+								const xhr: XMLHttpRequest = new XMLHttpRequest();
+								xhr.addEventListener("load", () =>{
+									this.cf.addRobotChatResponse("We received your submission 🙌");
+									setTimeout(() => success(), 2000);
+								});
+								xhr.open('POST', "https://api.formbucket.com/f/buk_YR7xGobkQQBP0bmXAWXtxn1B");
+								xhr.setRequestHeader("accept", "application/javascript");
+								xhr.setRequestHeader("Content-Type", "application/json");
+								xhr.send(JSON.stringify(this.cf.getFormData(true)));
+							}else{
+								success()
+							}
+						}else{
+							success()
+						}
 					}
 				});
 			}
