cad098641954018b6308cee9129697e55e62d9b5
Felix Nielsen <felix@flexmotion.dk>
2017-03-24 05:44:04 +0100

    refactor of gulp done — seperation of docs, examples and form, allow for * project seperation, soon * Travis to easily be implemented w. gulp tasks

--- examples/src/scripts/cf/ConversationalFormExamples.ts
+++ examples/src/scripts/cf/ConversationalFormExamples.ts
@@ -1,7 +1,7 @@
 // Docs version 1.0.0
 
 interface Window {
-	conversationalFormDocs: any;
+	conversationalFormExamples: any;
 }
 
 // declare module cf{
@@ -17,10 +17,8 @@ interface Window {
 // declare var ConversationalForm: any;
 class ConversationalForm {}
 
-class ConversationalFormDocs{
-	private static instance: ConversationalFormDocs
-
-	private cf: any;
+class ConversationalFormExamples{
+	private static instance: ConversationalFormExamples
 	/**
 	* 
 	* @type HTMLElement
@@ -101,43 +99,6 @@ class ConversationalFormDocs{
 		clearTimeout(this.introTimer);
 
 		if(!this.el.classList.contains('cf-toggle')){
-
-			if(!this.cf){
-				this.cf = new (<any> window).cf.ConversationalForm({
-					formEl: document.getElementById("cf-form"),
-					context: document.getElementById("cf-context"),
-					robotImage: "data:image/svg+xml;charset=utf-8;base64,PHN2ZyB3aWR0aD0iMjBweCIgaGVpZ2h0PSIyMHB4IiB2aWV3Qm94PSIwIDAgMjAgMjAiIHZlcnNpb249IjEuMSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayI+CiAgICA8ZyBzdHJva2U9Im5vbmUiIHN0cm9rZS13aWR0aD0iMSIgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj4KICAgICAgICA8ZyB0cmFuc2Zvcm09InRyYW5zbGF0ZSgtNzY0LjAwMDAwMCwgLTUzMC4wMDAwMDApIiBmaWxsPSIjMjIyMjIyIj4KICAgICAgICAgICAgPGcgdHJhbnNmb3JtPSJ0cmFuc2xhdGUoNzUzLjAwMDAwMCwgNTE5LjAwMDAwMCkiPgogICAgICAgICAgICAgICAgPHJlY3QgeD0iMTEiIHk9IjExIiB3aWR0aD0iMjAiIGhlaWdodD0iMjAiPjwvcmVjdD4KICAgICAgICAgICAgPC9nPgogICAgICAgIDwvZz4KICAgIDwvZz4KPC9zdmc+",
-					userImage: "data:image/svg+xml;charset=utf-8;base64,PHN2ZyB3aWR0aD0iMjBweCIgaGVpZ2h0PSIxNnB4IiB2aWV3Qm94PSIwIDAgMjAgMTYiIHZlcnNpb249IjEuMSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayI+CiAgICA8ZyBzdHJva2U9Im5vbmUiIHN0cm9rZS13aWR0aD0iMSIgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj4KICAgICAgICA8ZyB0cmFuc2Zvcm09InRyYW5zbGF0ZSgtMTM3MS4wMDAwMDAsIC02MTAuMDAwMDAwKSIgZmlsbD0iI0ZGRkZGRiI+CiAgICAgICAgICAgIDxnIHRyYW5zZm9ybT0idHJhbnNsYXRlKDEyNzQuMDAwMDAwLCA1OTkuMDAwMDAwKSI+CiAgICAgICAgICAgICAgICA8cG9seWdvbiBwb2ludHM9IjEwNyAxMSAxMTcgMjcgOTcgMjciPjwvcG9seWdvbj4KICAgICAgICAgICAgPC9nPgogICAgICAgIDwvZz4KICAgIDwvZz4KPC9zdmc+",
-					submitCallback: () => {
-						
-					},
-					flowStepCallback: (dto: any, success: () => void, error: () => void,) => {
-						if(dto.input.currentTag.domElement){
-							if(dto.input.currentTag.domElement.getAttribute("name") == "repeat"){
-								location.reload();
-							}else if(dto.input.currentTag.domElement.getAttribute("name") == "submit-form"){
-								const xhr: XMLHttpRequest = new XMLHttpRequest();
-								xhr.addEventListener("load", () =>{
-									this.cf.addRobotChatResponse("We received your submission 🙌");
-									success();
-								});
-								xhr.open('POST', document.getElementById("cf-form").getAttribute("action"));
-								xhr.setRequestHeader("accept", "application/javascript");
-								xhr.setRequestHeader("Content-Type", "application/json");
-								xhr.send(JSON.stringify(this.cf.getFormData(true)));
-							}else{
-								success()
-							}
-						}else{
-							success()
-						}
-					}
-				});
-			}
-
-			if(this.cf.focus)
-				this.cf.focus()
-
 			setTimeout(() =>{
 				this.el.classList.remove('menu-toggle');
 				this.el.classList.add('cf-toggle')
@@ -146,13 +107,12 @@ class ConversationalFormDocs{
 			this.el.classList.remove('cf-toggle');
 		}
 
-
 		return false;
 	}
 
 	public static start(){
-		if(!ConversationalFormDocs.instance)
-			window.conversationalFormDocs = new ConversationalFormDocs();
+		if(!ConversationalFormExamples.instance)
+			window.conversationalFormExamples = new ConversationalFormExamples();
 	}
 }
 
@@ -214,10 +174,10 @@ class H1Writer{
 
 if(document.readyState == "complete"){
 	// if document alread instantiated, usually this happens if Conversational Form is injected through JS
-	ConversationalFormDocs.start();
+	ConversationalFormExamples.start();
 }else{
 	// await for when document is ready
 	window.addEventListener("load", () =>{
-		ConversationalFormDocs.start();
+		ConversationalFormExamples.start();
 	}, false);
 }
