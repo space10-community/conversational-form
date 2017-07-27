b9507db18b1f4f88089c5ce6058d683076c74f41
Felix Nielsen <felix.nielsen@rwatgg.dk>
2017-07-27 13:41:56 +0100

    setup some initial tests for input type text

--- tests/reference-check.js
+++ tests/reference-check.js
@@ -1,5 +1,15 @@
-describe('Check Conversational Form reference', function() {
+describe('Check Conversational Form in global scope', function() {
 	it('Should exist', function() {
 		expect(window.cf.ConversationalForm).not.toBeNull();
+	});
+});
+
+describe('Check Conversational Form reference', function() {
+	it('Should exist', function() {
+		var instance = window.cf.ConversationalForm.startTheConversation({
+			formEl: document.createElement("form")
+		});
+
+		expect(instance).not.toBeNull();
 	});
 });
