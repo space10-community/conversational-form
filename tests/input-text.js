977d3fa7b07ec51319a74797b066bb07a2bd7e40
Felix Nielsen <felix.nielsen@rwatgg.dk>
2017-07-27 15:53:39 +0100

    add more test cases for input[type=text] remove error handler from ChatList as the error is not acted upon.

--- tests/input-text.js
+++ tests/input-text.js
@@ -6,8 +6,16 @@ var formless = {
 		{
 			"tag": "input",
 			"type": "text",
+			"name": "first-tag",
 			"id": "first-tag",
-			"value": "Prefilled value here",
+			"value": "test",
+			"cf-questions": "Prefilled1&&with follow-up1&&with follow-up11||Prefilled2&&with follow-up2&&with follow-up22"
+		},
+		{
+			"tag": "input",
+			"type": "text",
+			"name": "second-tag",
+			"id": "second-tag",
 			"cf-questions": "Prefilled1&&with follow-up1&&with follow-up11||Prefilled2&&with follow-up2&&with follow-up22"
 		}
 	]
@@ -15,10 +23,25 @@ var formless = {
 
 var instance = window.cf.ConversationalForm.startTheConversation(formless);
 
-describe('Check input type text', function() {
-	it('Should contain tag', function() {
+describe('Check input type=text', function() {
+	it('Should be able to through all', function() {
 		expect(instance.tags).not.toBeNull();
+		expect(instance.tags.length).toBe(2);
 		expect(instance.tags[0].id).toBe("first-tag");
 		expect(instance.tags[0].domElement).not.toBeNull();
+
+		expect(instance.tags[0].value).toBe("test");
+		// add value and submit
+		instance.userInput.inputElement.value = "Hello world";
+		instance.userInput.doSubmit();
+
+		// testing tag value
+		expect(instance.tags[0].value).toBe("Hello world");
+		
+		// testing original tag (dom element) value
+		expect(instance.tags[0].domElement.value).toBe("Hello world");
+
+		// testing getFormData
+		expect(instance.getFormData(true)["first-tag"]).toBe("Hello world");
 	});
 });
