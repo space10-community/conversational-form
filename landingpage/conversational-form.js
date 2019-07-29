26c9176b8b85d8c06250ac8a071b99feecf5cf4d
Jens Soegaard <jens@jenssogaard.com>
2019-07-29 09:59:44 -0400

    New build

--- landingpage/conversational-form.js
+++ landingpage/conversational-form.js
@@ -5728,7 +5728,7 @@ var cf;
 (function (cf_1) {
     var ConversationalForm = /** @class */ (function () {
         function ConversationalForm(options) {
-            this.version = "0.9.90";
+            this.version = "1.0.0";
             this.cdnPath = "https://cdn.jsdelivr.net/gh/space10-community/conversational-form@{version}/dist/";
             this.isDevelopment = false;
             this.loadExternalStyleSheet = true;
